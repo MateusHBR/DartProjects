dcf8d2e1b6812852ac8b701ef82a7c7abc72d3a5
MateusHBR <mateusmorais.r@gmail.com>
2019-11-02 12:00:35 -0300

    adicionado uma lista de maps.

--- cadastro_cliente.dart
+++ cadastro_cliente.dart
@@ -1,19 +1,20 @@
 import 'dart:io';
 
-Map<String, dynamic> cadastro = {};
+List<Map<String, dynamic>> cadastros = [];
 
 main() {
   bool trab = true;
   String resposta;
   while (trab) {
-    print("\n \n \ncadastrar, listar ou sair?");
+    print("\n \nDigite um comando:\ncadastrar, listar ou sair.");
     resposta = stdin.readLineSync();
     if (resposta == "cadastrar") {
       trabalho();
     } else if (resposta == "sair") {
       trab = false;
+      print(cadastros);
     } else if (resposta == "listar") {
-      print(cadastro);
+      print(cadastros);
     } else {
       print("Opcao invalida\n");
     }
@@ -21,6 +22,7 @@ main() {
 }
 
 trabalho() {
+  Map<String, dynamic> cadastro = {};
   print("Informe seu nome: ");
   cadastro["nome"] = stdin.readLineSync();
 
@@ -29,4 +31,5 @@ trabalho() {
 
   print("Informe seu estado: ");
   cadastro["endereco"] = stdin.readLineSync();
+  cadastros.add(cadastro);
 }
