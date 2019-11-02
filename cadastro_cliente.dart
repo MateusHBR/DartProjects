7b10f5e970f2dbab80f088db89e35b35647d5ab1
MateusHBR <mateusmorais.r@gmail.com>
2019-11-02 11:54:30 -0300

    adicionando cadastrar, sair e listar.

--- cadastro_cliente.dart
+++ cadastro_cliente.dart
@@ -3,7 +3,21 @@ import 'dart:io';
 Map<String, dynamic> cadastro = {};
 
 main() {
-  trabalho();
+  bool trab = true;
+  String resposta;
+  while (trab) {
+    print("\n \n \ncadastrar, listar ou sair?");
+    resposta = stdin.readLineSync();
+    if (resposta == "cadastrar") {
+      trabalho();
+    } else if (resposta == "sair") {
+      trab = false;
+    } else if (resposta == "listar") {
+      print(cadastro);
+    } else {
+      print("Opcao invalida\n");
+    }
+  }
 }
 
 trabalho() {
@@ -14,4 +28,5 @@ trabalho() {
   cadastro["idade"] = int.parse(stdin.readLineSync());
 
   print("Informe seu estado: ");
+  cadastro["endereco"] = stdin.readLineSync();
 }
